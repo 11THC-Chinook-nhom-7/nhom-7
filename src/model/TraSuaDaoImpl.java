75bdc507c84a696a38269c45d9a82f4c6701ca87
= <=>
2020-01-04 15:17:01 +0700

    up code

--- src/model/TraSuaDaoImpl.java
+++ src/model/TraSuaDaoImpl.java
@@ -6,23 +6,47 @@ import java.util.List;
 
 public class TraSuaDaoImpl implements TraSuaDao {
 
-    public TraSuaDaoImpl()
-    {
+    public TraSuaDaoImpl() {
         Database db = new Database();
         String SQL_CREATE_TRASUA_TABLE = "CREATE TABLE IF NOT EXISTS tbl_TraSua (\n"
                 + "    ID integer PRIMARY KEY,\n"
-                + "    nameTraSua text NOT NULL,\n"
+                + "    name text NOT NULL,\n"
                 + "    price money NOT NULL,\n"
-                + "    qty integer NOT NULL \n"
+                + "    size text  NOT NULL,\n"
+                + "    quality integer NOT NULL \n"
                 + ");";
 
-        try
-        {
-            Statement statement =db.getConnection().createStatement();
+        String SQL_CREATE_TOPPING_TABLE = "CREATE TABLE IF NOT EXISTS tbl_TopPing (\n"
+                + "    ID_topping integer PRIMARY KEY,\n"
+                + "    name text NOT NULL,\n"
+                + "    price money NOT NULL,\n"
+                + "    quality integer NOT NULL \n"
+                + ");";
+
+
+        String SQL_CREATE_HOADON_TABLE = "CREATE TABLE IF NOT EXISTS tbl_HOADON (\n"
+                + "    id_hoadon  integer PRIMARY KEY,\n"
+                + "    ID integer ,\n"
+                + "    nameTraSua text NOT NULL,\n"
+                + "    size text NOT NULL,\n"
+                + "    price money NOT NULL,\n"
+                + "    qty integer NOT NULL, \n"
+                + "    total money NOT NULL, \n"
+                + "    note text  \n"
+                + ");";
+
+        String SQL_CREATE_DETAIL_TABLE = "CREATE TABLE IF NOT EXISTS tbl_Detail (\n"
+                + "    ID_Detail integer PRIMARY KEY,\n"
+                + "    id_hoadon integer NOT NULL,\n"
+                + "    ID_topping integer NOT NULL \n"
+                + ");";
+
+        try {
+            Statement statement = db.getConnection().createStatement();
             statement.execute(SQL_CREATE_TRASUA_TABLE);
-        }
-        catch (SQLException e)
-        {
+            statement.execute(SQL_CREATE_TOPPING_TABLE);
+            statement.execute(SQL_CREATE_HOADON_TABLE);
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
